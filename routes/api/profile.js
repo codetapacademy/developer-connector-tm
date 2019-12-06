19fceef242b6e12c921c1cf6c270f36adf5ee523
Bogdan Negura <negura.bogdan.90@gmail.com>
2019-12-06 21:07:03 +0000

    Delete Profile and User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -143,4 +143,23 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route   Delete api/profile
+// @desc   Delete profile, user and posts
+// @access   Private
+router.delete("/", auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove User
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.err(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
